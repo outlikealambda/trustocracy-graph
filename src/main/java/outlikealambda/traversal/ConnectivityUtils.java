bf3864c5d0113c5d10f0453642a914e843578725
Will Reppun <(none)>
2017-01-15 21:32:23 -1000

    :toilet: remove unused imports

--- src/main/java/outlikealambda/traversal/ConnectivityUtils.java
+++ src/main/java/outlikealambda/traversal/ConnectivityUtils.java
@@ -8,9 +8,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Stream;
 
 public final class ConnectivityUtils {
