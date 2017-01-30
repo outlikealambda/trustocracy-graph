bddf190781967e6eea0e457555c7c4e3e97f40d1
Will Reppun <(none)>
2017-01-29 22:12:43 -1000

    :seedling: add builder for testing walker graphs

--- src/test/java/outlikealambda/traversal/TestUtils.java
+++ src/test/java/outlikealambda/traversal/TestUtils.java
@@ -71,4 +71,60 @@ public class TestUtils {
 			return String.format("(%s)-[:%s]->(%s)", source, r, target);
 		}
 	}
+
+	public static Walkable createWalkable(int topicId) {
+		return new Walkable(topicId);
+	}
+
+	public static class Walkable {
+		private List<String> creates = new ArrayList<>();
+		private final int topicId;
+
+		public Walkable(int topicId) {
+			this.topicId = topicId;
+		}
+
+		public String build() {
+			return "CREATE" + creates.stream().collect(Collectors.joining(","));
+		}
+
+		public Walkable addPerson(String person, int id) {
+			creates.add(String.format("(%s:Person {name:'%s', id:%d})", person, person, id));
+			return this;
+		}
+
+		public Walkable addDisjointPerson(String person, int id) {
+			creates.add(String.format("(%s:Person {name:'%s', id:%d, disjoint:'true'})", person, person, id));
+			return this;
+		}
+
+		public Walkable addOpinion(String opinion, int id) {
+			creates.add(String.format("(%s:Opinion {id:%d})", opinion, id));
+			return this;
+		}
+
+		public Walkable connectManual(String source, String target) {
+			creates.add(connect(source, target, RelationshipTypes.manual(topicId).name()));
+			return this;
+		}
+
+		public Walkable connectAuthored(String source, String target) {
+			creates.add(connect(source, target, RelationshipTypes.authored(topicId).name()));
+			return this;
+		}
+
+		public Walkable connectRanked(String source, String target, int rank) {
+			creates.add(connect(source, target, String.format("RANKED {rank:%d}", rank)));
+			return this;
+		}
+
+		public Walkable connectConnected(String source, String target) {
+			creates.add(connect(source, target, RelationshipTypes.connected(topicId).name()));
+			return this;
+		}
+
+		private static String connect(String source, String target, String r) {
+			return String.format("(%s)-[:%s]->(%s)", source, r, target);
+		}
+	}
 }
