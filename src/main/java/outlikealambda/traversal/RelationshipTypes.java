2ff56420dbf30e6cc02ed7e16dacd4b55564f3a5
Will Reppun <reppun@gmail.com>
2017-02-09 15:48:40 -1000

    :toilet: remove old RelationshipTypes

--- src/main/java/outlikealambda/traversal/RelationshipTypes.java
+++ src/main/java/outlikealambda/traversal/RelationshipTypes.java
@@ -4,9 +4,7 @@ import org.neo4j.graphdb.RelationshipType;
 
 public final class RelationshipTypes {
 	private static final String MANUAL = "MANUAL";
-	private static final String PROVISIONAL = "PROVISIONAL";
 	private static final String AUTHORED = "AUTHORED";
-	private static final String ONCE_AUTHORED = "ONCE_AUTHORED";
 	private static final String RANKED = "RANKED";
 	private static final String CONNECTED = "CONNECTED";
 
@@ -16,16 +14,8 @@ public final class RelationshipTypes {
 		return combineTypeAndId(MANUAL, topic);
 	}
 
-	static RelationshipType provisional(long topic) {
-		return combineTypeAndId(PROVISIONAL, topic);
-	}
-
 	public static RelationshipType authored(long topic) {
 		return combineTypeAndId(AUTHORED, topic);
-	}
-
-	static RelationshipType onceAuthored(long topic) {
-		return combineTypeAndId(ONCE_AUTHORED, topic);
 	}
 
 	public static RelationshipType connected(long topic) {
